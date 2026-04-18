8473990001430776851bc8e543bd5ed2d9767512
Alexey <lp@tdsgn.ru>
2026-04-18 11:56:41 +0700

    feat: add coordinate axes and scale labels to debug charts

--- src/Console/Commands/HiveDebugChartCommand.php
+++ src/Console/Commands/HiveDebugChartCommand.php
@@ -38,21 +38,37 @@ final class HiveDebugChartCommand extends Command
     {
         $this->info("\n--- Переходный процесс (Нагрузка [█] vs Отсечение [░]) ---");
         
+        // Сетка + место под ось Y (4 символа слева)
+        $yAxisWidth = 5;
         $grid = array_fill(0, $h, array_fill(0, $w, ' '));
+        $data = $data->values();
 
-        foreach ($data->values() as $x => $point) {
+        foreach ($data as $x => $point) {
             $yHealth = (int)(($point->avg_health ?? 0) / 100 * ($h - 1));
-            $grid[$h - 1 - $yHealth][$x] = '█';
-
             $yPid = (int)(($point->shedding_rate ?? 0) / 100 * ($h - 1));
+
+            $grid[$h - 1 - $yHealth][$x] = '█';
+            
+            // Рисуем сигнал ПИД, если место не занято нагрузкой
             if ($grid[$h - 1 - $yPid][$x] === ' ') {
                 $grid[$h - 1 - $yPid][$x] = '░';
             }
         }
 
-        foreach ($grid as $row) {
-            $this->line(implode('', $row));
+        // Отрисовка с осью Y
+        foreach ($grid as $y => $row) {
+            $label = match($y) {
+                0 => '100%',
+                (int)($h/2) => ' 50%',
+                $h-1 => '  0%',
+                default => '    '
+            };
+            $this->line("<fg=gray>{$label} ┨</>" . implode('', $row));
         }
+
+        // Отрисовка оси X
+        $xAxis = str_repeat('━', $w);
+        $this->line(str_repeat(' ', $yAxisWidth) . "<fg=gray>┗{$xAxis}▶ Time</>");
     }
 
     private function renderHysteresis($data, $h): void
@@ -64,12 +80,19 @@ final class HiveDebugChartCommand extends Command
         foreach ($data as $point) {
             $x = (int)(($point->avg_health ?? 0) / 100 * ($canvasSize - 1));
             $y = (int)(($point->shedding_rate ?? 0) / 100 * ($h - 1));
-            
             $grid[max(0, $h - 1 - $y)][max(0, $x)] = '•';
         }
 
-        foreach ($grid as $row) {
-            $this->line(implode('', $row));
+        foreach ($grid as $y => $row) {
+            $label = match($y) {
+                0 => '100',
+                (int)($h/2) => ' 50',
+                $h-1 => '  0',
+                default => '   '
+            };
+            $this->line("<fg=gray>{$label} ┨</>" . implode('', $row));
         }
+        $this->line(str_repeat(' ', 4) . "┗" . str_repeat('━', $canvasSize) . "▶ Load %");
     }
+
 }
